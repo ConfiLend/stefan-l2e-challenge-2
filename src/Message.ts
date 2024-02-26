23f45c40a7cacde1a05d279329025dd9fbf3f8ad
Stefan Madzharov <stefanmadzharov@gmail.com>
2024-02-26 15:06:09 +0000

    The structs are ready

--- src/Message.ts
+++ src/Message.ts
@@ -1,31 +1,59 @@
 import {
-  Field,
-  MerkleMapWitness,
-  Struct,
+    Field,
+    Struct,
+    Bool
 } from 'o1js';
 
-export class RollupState extends Struct({
-    initialRoot: Field,
-    latestRoot: Field,
-  }) { 
-  
-    static createOneStep(
-      initialRoot: Field, 
-      latestRoot: Field, 
-      key: Field, 
-      currentValue: Field,
-      incrementAmount: Field, 
-      merkleMapWitness: MerkleMapWitness,
-    ) {
-      const [ witnessRootBefore, witnessKey ] = merkleMapWitness.computeRootAndKey(currentValue);
-      initialRoot.assertEquals(witnessRootBefore);
-      witnessKey.assertEquals(key);
-      const [ witnessRootAfter, _ ] = merkleMapWitness.computeRootAndKey(currentValue.add(incrementAmount));
-      latestRoot.assertEquals(witnessRootAfter);
-  
-      return new RollupState({
-        initialRoot,
-        latestRoot
-      });
+class MessageDetails extends Struct({
+    AgentID: Field,
+    AgentXLocation: Field,
+    AgentYLocation: Field,
+    CheckSum: Field
+}) {
+
+    checkConditions(): Bool {
+        // checkSum is correct
+        let checkSumCheck = this.checkCheckSum();
+        // ranges are correct
+        let rangeCheck = this.checkRanges();
+        // check YLocation > XLocation
+        let coordinateCheck = this.checkCoordinates();
+
+        return checkSumCheck.and(rangeCheck).and(coordinateCheck)
+            // if AgentID = 0 we don't check anything more
+            .or(this.AgentID.equals(0));
+    }
+
+    // checking if all the ranges are right
+    checkRanges(): Bool {
+        let agentId = MessageDetails.inRange(this.AgentID, Field(0), Field(3000));
+        let XLocation = MessageDetails.inRange(this.AgentXLocation, Field(0), Field(15000));
+        let YLocation = MessageDetails.inRange(this.AgentYLocation, Field(5000), Field(20000));
+        return agentId.and(XLocation).and(YLocation);
+    }
+
+    // check if the checkSum is right
+    checkCheckSum(): Bool {
+        return this.AgentID.add(this.AgentXLocation).add(this.AgentYLocation).equals(this.CheckSum);
+    }
+
+    // check if the checkSum is right
+    checkCoordinates(): Bool {
+        return this.AgentYLocation.greaterThan(this.AgentYLocation);
+    }
+
+    // check if a value is in a specific range
+    static inRange(value: Field, lowerBound: Field, upperBound: Field): Bool {
+        return value.greaterThanOrEqual(lowerBound).and(value.lessThanOrEqual(upperBound))
     }
 }
+
+export class Message extends Struct({
+    MessageNumber: Field,
+    MessageDetails: MessageDetails
+}) {
+    checkMessgeDetails(): Bool {
+        return this.MessageDetails.checkConditions();
+    }
+}
+
